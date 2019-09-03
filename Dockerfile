452c0512faee10e0f1731c790b5a2e37272c0c2d
ed kim <edward.kim@lendi.com.au>
2019-09-03 10:20:30 +1000

    First round of refactoring of the script.
    Got rid of the useless stuff and we're using env variables instead of a
    static configuration file.
    
    Added logging and removed statsd and instead, push the metrics directly to the API.
    
    Building this into a docker image.
    
    Signed-off-by: ed kim <edward.kim@lendi.com.au>

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.7.4-alpine3.10
+
+# Generic container setup
+WORKDIR /usr/app
+RUN addgroup -g 1001 -S appgroup && \
+adduser -u 1001 -S appuser -G appgroup && \
+chown -R appuser:appgroup /usr/app
+USER appuser
+
+# Dependency setup
+ADD requirements.txt .
+RUN pip install -r requirements.txt --user
+
+# App setup
+ENV DD_API_KEY DD_APP_KEY
+ADD ic2datadog.py .
+
+CMD ["python", "ic2datadog.py"]
--- ic2datadog.py
+++ ic2datadog.py
@@ -1,43 +1,75 @@
 #!/usr/bin/env python
 __author__ = 'ben.slater@instaclustr.com'
 
-from datadog import initialize
+from datadog import initialize, api
 from time import sleep
-from datadog import statsd
-import requests, json
+from datetime import datetime
+import requests, json, json_logging, logging
 from requests.auth import HTTPBasicAuth
-import os
+import os, signal, sys
 
-configFile = os.path.dirname(os.path.realpath(__file__)) + "/configuration.json"
-f = open(configFile)
-configuration = json.loads(f.read())
-f.close()
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
-dd_options = configuration['dd_options']
+# Environment variable setup
+default_value = ''
+app_name = os.getenv('APP_NAME', 'instaclustr-monitor')
+log_level = logging.getLevelName(os.getenv('LOG_LEVEL', 'DEBUG').upper())
+ic_cluster_id = os.getenv('IC_CLUSTER_ID', default_value)
+ic_metrics_list = os.getenv('IC_METRICS_LIST',
+'k::slaConsumerRecordsProcessed,n::cpuutilization,n::diskUtilization,\
+n::osLoad,k::kafkaBrokerState,k::slaProducerErrors,k::slaConsumerLatency,\
+k::slaProducerLatencyMs,k::underReplicatedPartitions,k::activeControllerCount,\
+k::offlinePartitions,k::leaderElectionRate,k::uncleanLeaderElections,\
+k::leaderCount,k::isrExpandRate,k::isrShrinkRate')
+ic_user_name = os.getenv('IC_USER_NAME', default_value)
+ic_api_key = os.getenv('IC_API_KEY', default_value)
+## IC_TAGS should be a comma separated list of strings, e.g. tag1:this,tag2:that
+ic_tags = os.getenv('IC_TAGS', 'environment:development').split(',')
 
-initialize(**dd_options)
+# Toggle the below with env variable ENABLE_JSON_LOGGING
+# json_logging.ENABLE_JSON_LOGGING = False
+json_logging.init_non_web()
 
-auth_details = HTTPBasicAuth(username=configuration['ic_options']['user_name'], password=configuration['ic_options']['api_key'])
+logger = logging.getLogger(app_name)
+logger.setLevel(log_level)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
+# Assuming you've set `DD_API_KEY` and `DD_APP_KEY` in your env,
+# initialize() will pick it up automatically
+initialize()
+
+auth_details = HTTPBasicAuth(username=ic_user_name, password=ic_api_key)
 
 consecutive_fails = 0
+target = 'https://api.instaclustr.com/monitoring/v1/clusters/{}?metrics={}'.format(ic_cluster_id, ic_metrics_list)
+logger.debug(target)
+
+epoch = datetime(1970, 1, 1)
+myformat = "%Y-%m-%dT%H:%M:%S.%fZ"
 while True:
-    response = requests.get(url="https://api.instaclustr.com/monitoring/v1/clusters/{0}?metrics={1},".format(configuration['cluster_id'], configuration['metrics_list']), auth=auth_details)
+    response = requests.get(url=target, auth=auth_details)
 
     if not response.ok:
         # got an error response from the Instaclustr API - raise an alert in DataDog after 3 consecutive fails
         consecutive_fails += 1
-        print "Error retrieving metrics from Instaclustr API: {0} - {1}".format(response.status_code, response.content)
+        logger.Error('Error retrieving metrics from Instaclustr API: {0} - {1}'.format(response.status_code, response.content))
+        logger.debug(response)
         if consecutive_fails > 3:
-            statsd.event("Instaclustr monitoring API error", "Error code is: {0}".format(response.status_code))
+            logger.Fatal("Instaclustr monitoring API error", "Error code is: {0}".format(response.status_code))
         sleep(20)
         continue
 
     consecutive_fails = 0
     metrics = json.loads(response.content)
+    logger.info('Retrieve metrics from instaclustr ok')
     for node in metrics:
-        public_ip = node["publicIp"]
+        send_list = []
+        id = node["id"] or ''
+        public_ip = node["publicIp"] or ''
         private_ip = node["privateIp"]
         rack_name = node["rack"]["name"]
         data_centre_custom_name = node["rack"]["dataCentre"]["customDCName"]
@@ -46,7 +78,8 @@ while True:
         provider_account_name = node["rack"]["providerAccount"]["name"]
         provider_account_provider = node["rack"]["providerAccount"]["provider"]
 
-        tag_list = ['ic_public_ip:' + public_ip,
+        tag_list = ['ic_cluster_id:' + id,
+                    'ic_public_ip:' + public_ip,
                     'ic_private_ip:' + private_ip,
                     'ic_rack_name:' + rack_name,
                     'ic_data_centre_custom_name:' + data_centre_custom_name,
@@ -62,17 +95,21 @@ while True:
             ]
 
         for metric in node["payload"]:
-            dd_metric_name = 'instaclustr.{0}'.format(metric["metric"])
-            if metric["metric"] == "nodeStatus":
-                # node status metric maps to a data dog service check
-                if metric["values"][0]["value"] =="WARN":
-                    statsd.service_check(dd_metric_name, 1, tags=configuration['tags'] + tag_list) # WARN status
-                else:
-                    statsd.service_check(dd_metric_name, 0, tags=configuration['tags'] + tag_list) # OK status
-            else:
-                # all other metrics map to a data dog guage
-                statsd.gauge(dd_metric_name, metric["values"][0]["value"], tags=configuration['tags'] + tag_list)
+            dd_metric_name = 'instaclustr.{0}.{1}'.format(metric["metric"],metric["type"])
+            mydt = datetime.strptime(metric["values"][0]["time"], myformat)
+            time_val= int((mydt - epoch).total_seconds())
+            logger.debug(metric)
+            logger.debug(dd_metric_name)
+            send_list.append({'metric' : dd_metric_name, 'points' : [(time_val,float(metric["values"][0]["value"]))],'tags' : ic_tags + tag_list})
+
+        # Sends metrics per node as per tagging rules.
+        if (send_list):
+            logger.debug('Sending: {0}'.format(send_list))
+            dd_response = api.Metric.send(send_list)
+            if dd_response['status'] != 'ok':
+                logger.fatal('Error sending metrics to DataDog: {0}'.format(dd_response))
+            logger.info('Sent metrics of node {0} to DataDog API with response: {1}'.format(id, dd_response['status']))
+        else:
+            logger.info('Empty list from the instaclustr API for the node: {0}'.format(id))
 
     sleep(20)
-
-
